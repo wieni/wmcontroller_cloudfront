59f914107acfb9648f7f508eabd834ea7c2c64e1
Robin Houtevelts <robin@wieni.be>
2019-02-22 11:34:36 +0100

    Further upstream changes

--- src/CloudFront.php
+++ src/CloudFront.php
@@ -13,7 +13,6 @@ class CloudFront implements StorageInterface
     protected $storage;
 
     protected $concurrent = 50;
-    protected $killSwitch = false;
 
     public function __construct(
         CloudFrontInvalidator $invalidator,
@@ -31,42 +30,22 @@ class CloudFront implements StorageInterface
 
     public function flush()
     {
-        // Flag the killSwitch. It's possible that the backend storage
-        // calls ::remove() a bunch of times to clear. We don't want to be
-        // invalidating those calls. When all is well and done we'll do a
-        // mass-invalidation of all pages with "/*"
-        $this->killSwitch = true;
-
         $this->storage->flush();
-
-        $this->killSwitch = false;
-
         $this->invalidator->invalidate(['/*']);
-    }
-
-    public function removeExpired($amount)
-    {
-        // Flag the killSwitch. We don't want to invalidate expired paths as
-        // the path is most likely also expired at the CDN.
-        $this->killSwitch = true;
-
-        $this->storage->removeExpired($amount);
-
-        $this->killSwitch = false;
     }
 
     protected function invalidate(array $ids)
     {
-        if ($this->killSwitch) {
-            return;
-        }
-
         // Invalidate in a foreach loop so we can leverage generators and
         // play nice with our memory when invalidating a whole bunch of items.
         //
         // Even though CloudFront does max 3000 concurrent invalidations..
         $paths = [];
         foreach ($this->storage->loadMultiple($ids, false) as $item) {
+            /** @var Cache $item */
+            if ($item->getExpiry() < time() + 60) {
+                continue;
+            }
             $paths[] = parse_url($item->getUri(), PHP_URL_PATH);
 
             if (count($paths) === $this->concurrent) {
@@ -98,5 +77,10 @@ class CloudFront implements StorageInterface
     public function getByTags(array $tags)
     {
         return $this->storage->getByTags($tags);
+    }
+
+    public function getExpired($amount)
+    {
+        $this->storage->getExpired($amount);
     }
 }
